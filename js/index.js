1172d6db0fe955dbe2c780e4e7606c6b3db9fa81
AhmadjonNoyibjonov <ahmadjonnoyibjonov60@gmail.com>
2024-06-05 10:33:48 +0500

    project

---
+++ js/index.js
@@ -0,0 +1,196 @@
+// 1-masala
+
+// function massiv(arg) {
+//   let arr= [];
+//   for(let i = Math.floor(Math.random() * 10); i < 100; i++ ) {
+//       arr.push(i);
+//       if(arr.length == 5) {
+//         break;
+//       }
+//   }
+
+//   return arr;
+// }
+// console.log(massiv());
+
+// 2-masala
+// function array(argument) {
+//   let arr = [];
+//   arr.push("salom");
+//   arr.push(12);
+//   arr.push(true);
+
+//   arr.pop();
+
+//   let A =   arr.toString();
+
+//   return A;
+// }
+
+// console.log(array());
+
+// 3-masala
+// function array(argument) {
+//   let mass = [];
+//   mass.unshift("bye");
+//   mass.unshift(false);
+//   mass.unshift(29);
+//   mass.unshift("good");
+
+//   mass.reverse();
+//   let L = mass.length;
+
+//   return mass;
+// }
+
+// console.log(array());
+
+// 4-masala
+// Domla shartda biros xatolik ketibdi shekili. 2 ta emas bir nechta element qoshib qoyyabman.
+// function array(argument) {
+//   let mass = [];
+//   mass.unshift("salom");
+//   mass.unshift(12);
+//   mass.unshift(true);
+//   mass.unshift("xayr");
+
+//   mass.shift();
+
+//   let M = mass.join(" , ");
+
+//   return M;
+// }
+
+// console.log(array());
+
+// 5-masala
+// let mass = ["salom", 29, 12, true, "bye"];
+// mass.pop();
+
+// let L = mass.length;
+// let a = mass.at(0);
+
+// console.log(L);
+// console.log(a);
+
+// 6-masala
+// function array(argument) {
+//   let mass = ['hello', 24, 54, 9, false];
+
+//   mass.push('bye');
+
+//   let a = mass.at(-1);
+
+//   let result = `${a}`;
+
+//   return result;
+
+// }
+
+// console.log(array());
+
+// 7-masala
+// function array(argument) {
+//   let mass = [12, 43, "good", true, 25];
+
+//   mass.unshift(25, "salom");
+
+//   mass.reverse();
+
+//   let L = mass.length;
+
+//   return L;
+// }
+
+// console.log(array());
+
+// 8-masala
+
+// let mass = [8, 12, "hello", 24, true];
+
+// mass.shift();
+
+// let L = mass.length;
+
+// let A = mass.join(' ');
+
+// console.log(L);
+// console.log(A);
+
+// 9-masala
+// let mass = [12, 5, true];
+// mass.unshift('hello');
+// mass.push(24);
+// mass.push('bye');
+
+// let L = mass.length;
+// mass.reverse();
+// let A = mass.join(' ');
+
+// console.log(L);
+// console.log(mass);
+// console.log(A);
+
+// 10-masala
+// let mas = [12, 9, "good", true, "nice"];
+// mas.pop();
+// mas.shift();
+// let L = mas.length;
+// mas.reverse();
+
+// console.log(L);
+// console.log(mas);
+
+// 11-masala
+// let mass = ["yes", "no", 12, 29, "ok", true];
+
+// mass.reverse();
+
+// let A = mass.join("");
+// let L = A.length;
+
+// console.log(A);
+// console.log(L);
+
+// 12- masala
+// let arr1 = [12, "ok", 5];
+// let arr2 = [29, true, "yes"];
+// let A = arr1.concat(arr2);
+
+// let N = A.slice(1, 2);
+
+// N.push(A.slice(3, 4));
+// N.push(A.slice(5, 6));
+
+// let L = N.length;
+
+// console.log(L);
+
+// 13-masala
+// let arr = [12,24,6,54,'salom', true, 'ok'];
+
+// arr.splice(1,3);
+
+// let m = arr.join('');
+
+// let L = m.length;
+
+// console.log(L);
+
+// 14-masala
+// let arr = ["hello", 12, 24, "ok", true, 45];
+
+// let N = arr.slice(3,arr.length);
+
+// N.reverse();
+
+// console.log(N);
+
+// 15-masala
+// let arr = [12,24,29,'salom', [8,9], true, [0, 'ok', 'hello']];
+
+// let arr1 = arr.flat();
+ 
+// let n = arr1.at(-1);
+
+// console.log(n);
---
+++ js/index1.js
@@ -0,0 +1,37 @@
+// 1-masala
+// function summ(argument) {
+//   let max;
+//   max = argument[0];
+//   for (i = 0; i < argument.length; i++) {
+
+//     if (argument[i] > max) {
+//       max = argument[i];
+//     }
+//   }
+
+//   return max;
+// }
+
+// console.log(summ([2, 5, 3, 10, 0]));
+
+// 2-masala
+// function New(arg) {
+//     let a = [];
+//     for(i = 0; i < arg.length; i++) {
+//         if(arg[i] > 9 && arg[i] < 100) {
+//             a.push(arg[i]);
+//         }
+//     }
+
+//     return a;
+// }
+
+// console.log(New([2,4,12,-65,-32,24]));
+
+// 20-masala
+// const arr = [1, 2, 3, 4, 3, 2, 5];
+
+// function removeDuplicates(data) {
+  // return data.filter((value, index) => data.indexOf(value) === index);
+// }
+// console.log(removeDuplicates(arr));
